089633f7abcd0df30b2e59472442823fd7c83207
F3RN4ND02 <fernando.ujap@gmail.com>
2021-09-14 22:21:50 -0400

    just an init

---
+++ src/Components/start.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Start() {
+    return (
+        <div>
+            <h1>
+                Title to be the most @myself!
+            </h1>
+        </div>
+    )
+}
